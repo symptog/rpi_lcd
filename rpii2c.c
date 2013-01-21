19f57023acc4cfab74de92efebb39e61030dadb8
Tobias Gall <symptog@gmail.com>
2013-01-21 21:13:44 +0100

    added comments

--- rpii2c.c
+++ rpii2c.c
@@ -25,9 +25,18 @@
 
 #include "rpii2c.h"
 
-int fd;
+int fd; // FileDiscriptor
 
-void rpiI2cSetup(__u8 adr, __u8 busnum)
+/*
+ * 
+ * name: rpiI2cSetup
+ * @param __u8 addr (Address of I2C Device), __u8 busnum
+ * @return void
+ * 
+ * Sets up an I2C Device
+ * 
+ */
+void rpiI2cSetup(__u8 addr, __u8 busnum)
 {
 	
 	char *fileName;
@@ -38,16 +47,25 @@ void rpiI2cSetup(__u8 adr, __u8 busnum)
 		fileName = "/dev/i2c-1";
 	else 
 		exit(1);
-		 
+	// Open File for READ/WRITE
 	if((fd = open(fileName, O_RDWR)) < 0)
 		exit(1);
-   
-	if(ioctl(fd, I2C_SLAVE, adr) < 0) {               
+	// Sets Device as I2C Slave
+	if(ioctl(fd, I2C_SLAVE, addr) < 0) {               
 		close(fd);
 		exit(1);
 	}	
 }
 
+/*
+ * 
+ * name: rpiI2cWrite
+ * @param __u8 reg (Register), __u8 value (1 Byte Value)
+ * @return void
+ * 
+ * Writes Value to I2C Device on Register reg
+ * 
+ */
 void rpiI2cWrite(__u8 reg, __u8 value)
 {
 	if (i2c_smbus_write_byte_data(fd, reg, value) < 0) {
@@ -56,11 +74,15 @@ void rpiI2cWrite(__u8 reg, __u8 value)
 	}
 }
 
-void rpiI2cClose(void)
-{
-	close(fd);
-}
-
+/*
+ * 
+ * name: rpiI2cRead8
+ * @param __u8 reg (Register)
+ * @return __u8 (1 Byte Data)
+ * 
+ * Reads 1 Byte Data from I2C Device on Register reg
+ * 
+ */
 __u8 rpiI2cRead8(__u8 reg)
 {
 	__u8 res = i2c_smbus_read_byte_data(fd, reg);
@@ -68,6 +90,13 @@ __u8 rpiI2cRead8(__u8 reg)
 	return res;
 }
 
+/*
+ * 
+ * name: rpiI2cRead16
+ * @param __u8 reg (Register)
+ * @return __u16 (2 Byte Data)
+ * 
+ */
 __u16 rpiI2cRead16(__u8 reg)
 {
 	
@@ -77,4 +106,15 @@ __u16 rpiI2cRead16(__u8 reg)
 	return res;
 }
 
+/*
+ * 
+ * name: rpiI2cClose
+ * @param void
+ * @return void
+ * 
+ */
+void rpiI2cClose(void)
+{
+	close(fd);
+}
 
